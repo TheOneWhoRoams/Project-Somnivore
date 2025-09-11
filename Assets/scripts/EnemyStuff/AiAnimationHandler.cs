2a816e63d30da06539977f129b59f12ddef5ee41
Szef <EliaszWarzochaNetAcad@gmail.com>
2025-09-11 09:18:51 +0200

    Add AiAnimationHandler.cs

---
+++ Assets/scripts/EnemyStuff/AiAnimationHandler.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class AiAnimationHandler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
+
+    Animator AiAnimator;
+    void Start()
+    {
+        AiAnimator = GetComponent<Animator>();
+    }
+    
+    public void PlayAttack_CloseRange_1()
+    {
+        AiAnimator.SetTrigger("Attack_CloseRange_1");
+    }
+    public void PlayAttack_LongRange_Charge_1()
+    {
+        AiAnimator.SetTrigger("Attack_LongRange_Charge_1");
+    }
+    public void PlayDeath()
+    {
+        AiAnimator.SetTrigger("Death");
+    }
+    public void PlaySoftStagger()
+    {
+        AiAnimator.SetTrigger("SoftStagger");
+    }
+    public void PlayHardStagger()
+    {
+        AiAnimator.SetTrigger("HardStagger");
+    }
+    void Update()
+    {
+        
+    }
+}
